1c2e27c0c3f3d2efc73bd3718366c456aeeea383
Takahiro <tkhr_621@docomo.ne.jp>
2019-11-07 04:37:11 +0900

    Update

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  //<App />内のinnerHTMLに「こんにちわ」が含まれているかどうかをテストする
+  //<App />内のinnerHTMLに「みやだいもうくん」が含まれているかどうかをテストする
   //”toContain”は含まれているかどうかをチェックする関数
   expect(div.innerHTML).toContain('みやだいもうくん');
   ReactDOM.unmountComponentAtNode(div);
