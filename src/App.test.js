fc4994d3a12185123ad0d9cac81091ee493bb90c
Takahiro <tkhr_621@docomo.ne.jp>
2019-11-06 18:16:59 +0900

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,8 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  //<App />内のinnerHTMLに「こんにちわ」が含まれているかどうかをテストする
+  //”toContain”は含まれているかどうかをチェックする関数
+  expect(div.innerHTML).toContain('みやだいもうくん');
   ReactDOM.unmountComponentAtNode(div);
 });
