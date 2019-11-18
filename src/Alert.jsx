6ce78b1332cbcc2486faa9ae3a6f073063447f03
Takahiro <tkhr_621@docomo.ne.jp>
2019-11-18 19:47:17 +0900

    Update Alert.jsx

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Alert extends Component {
     constructor(props){
         super(props);
-        this.doAlert = this.doAlert.bind(this)
+        this.doAlert = this.doAlert.bind(this);
     }
 
     doAlert() {
-        alert(this.props.message)
+        alert(this.props.message);
     }
 
     render() {
